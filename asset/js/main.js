b53dd80665f4c384dfbf4fbfa82991ac75206ed6
KhuatCong <khuatphucong06092002@gmail.com>
2023-10-03 11:57:38 +0700

    update

--- asset/js/main.js
+++ asset/js/main.js
@@ -31,4 +31,75 @@ $(function () {
             }
         ]
     });
+
+    $('.list-item-product-sell').slick({
+        dots: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.img-to').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.img-nav',
+        draggable:false
+    });
+    $('.img-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.img-to',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true
+    });
 })
+
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, //khoảng cách đến khi bắt đầu hiện nút (px)
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop(); //lấy khoảng cách scroll hiện tại tính lên Top
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop(); //gọi function 'backToTop()' mỗi khi người dùng cuộn trang
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 400); //thời gian kéo lên top tính bằng mili giây 
+    });
+}
